0c7a991d3c5e6f5c4750274f208f114f44b90def
Sylvain81452 <47383150+Sylvain81452@users.noreply.github.com>
2020-05-12 00:15:53 +0200

    Revert "id as cookie key"
    This reverts commit 4ffcd02f4e4b2aec0febcb0b17b2e1cd25b68668.

--- src/app/php/login.php
+++ src/app/php/login.php
@@ -9,23 +9,12 @@
       $password = $_POST['password'];
 
       $sql = "SELECT Count(*) FROM users WHERE username = ? AND password = ?";
-      $sql2 = "SELECT * FROM users WHERE username = ?" ;
-
       $stmt = $conn->prepare($sql);
       $stmt -> bind_param("ss", $username,$password);
       $res = $stmt->execute();
 
-      $stmt->close();
-
-      $stmt = $conn->prepare($sql2);
-      $stmt->bind_param('s', $username);
-
       if($res) {
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $row = $result->fetch_assoc();
-        echo(json_encode($row["id"]));
-        
+        echo 1;
       }
       else {
         echo data;
